34729e43d42a2e1ac6cd7ce151ecdc20d5cbd95d
Charlie Jaime <baroonie10@gmail.com>
2016-12-04 03:25:26 -0800

    added main.jsx component

--- app/app.jsx
+++ app/app.jsx
@@ -1,10 +1,17 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var {Route, Router, IndexRoute, hashHistroy} = require('react-router');
+var Main = require('Main');
+
 
 /*********
 Dom Render
 **********/
 ReactDOM.render(
-  <h1>Weather App!</h1>,
+  <Router history={hashHistroy}>
+    <Route path='/' component={Main}>
+
+    </Route>
+  </Router>,
   document.getElementById("app")
 );
